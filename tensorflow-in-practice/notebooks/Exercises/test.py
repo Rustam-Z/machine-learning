be175a013e2298898cd1d5b5a5cf9c247f23ee99
Rustam-Z <zokirovrustam202@gmail.com>
2021-05-06 10:16:51 +0500

    new test images for MNIST

--- tensorflow-in-practice/notebooks/Exercises/test.py
+++ tensorflow-in-practice/notebooks/Exercises/test.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 model = tf.keras.models.load_model('/Users/rustamz/machine-learning/machine-learning-area/tensorflow-in-practice/notebooks/Exercises/my_model.h5')
 
-image = cv2.imread('/Users/rustamz/machine-learning/machine-learning-area/tensorflow-in-practice/img/3.jpg')
+image = cv2.imread('/Users/rustamz/machine-learning/machine-learning-area/tensorflow-in-practice/img/0.jpg')
 image = cv2.resize(image,(28,28))
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 data = np.vstack([gray])
